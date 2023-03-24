de43125853f672d4c6c9214cf7235896c305b7a9
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-03-23 22:15:49 -0700

    edit lab

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // State object keeps track of the application state (all available products and current state of the user's cart)
 const state = {
@@ -7,12 +7,12 @@ const state = {
 };
 
 // Cart constructor.
-const Cart = function(items) {
+const Cart = function (items) {
   // this.items is an array of CartItem instances.
   this.items = items;
 };
 
-Cart.prototype.addItem = function(product, quantity) {
+Cart.prototype.addItem = function (product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
   // let nextCartItem = new CartItem(addSelectedItemToCart[0], addSelectedItemToCart[1]);
   // state.cart.items.push(nextCartItem);
@@ -20,13 +20,14 @@ Cart.prototype.addItem = function(product, quantity) {
   this.items.push(nextCartItem);
 };
 
-Cart.prototype.saveToLocalStorage = function() {
-  // TODO: Fill in this instance method to save the contents of the cart to localStorage
+Cart.prototype.saveToLocalStorage = function () {
+  // TODO: Fill in this instance method to save the contents of the cart to localStorag
+
   let cart = JSON.stringify(this.items);
-  localStorage.setItem('cart', cart);
+  localStorage.setItem("cart", cart);
 };
 
-Cart.prototype.removeItem = function(item) {
+Cart.prototype.removeItem = function (item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
   // should we implement findIndex() and splice()
@@ -37,56 +38,78 @@ Cart.prototype.removeItem = function(item) {
   //loop over array
   //find index of item state.cart.items.findIndex(removedItem)
   //pass index into splice()
-  
 
+  console.log(this.items.splice(item, 1));
+  this.items.splice(item, 1);
+  state.cart.saveToLocalStorage();
 };
 
-Cart.prototype.updateCounter = function() {
+Cart.prototype.updateCounter = function () {
   // TODO: Update the cart count in the header nav with the number of items in the Cart
-  let cartCount = document.getElementById('itemCount');
-  
+  let cartCount = document.getElementById("itemCount");
+
   let totalItems = 0;
-  for(let i = 0; i < this.items.length; i++){
+  for (let i = 0; i < this.items.length; i++) {
     totalItems += parseInt(this.items[i].quantity);
     // console.log(typeof(totalItems));
   }
   // console.log(totalItems);
-  cartCount.textContent = ` (${totalItems})`;
-}
+  cartCount.textContent = ` ${totalItems}`;
+};
 
 // CartItem constructor
-const CartItem = function(product, quantity) {
+const CartItem = function (product, quantity) {
   this.product = product;
   this.quantity = quantity;
 };
 
 // Product contructor
-const Product = function(filePath, name) {
+const Product = function (filePath, name) {
   this.filePath = filePath;
   this.name = name;
 };
 
 function generateCatalog() {
-  let bag = new Product('assets/bag.jpg', 'Bag');
-  let banana = new Product('assets/banana.jpg', 'Banana');
-  let bathroom = new Product('assets/bathroom.jpg', 'Bathroom');
-  let boots = new Product('assets/boots.jpg', 'Boots');
-  let breakfast = new Product('assets/breakfast.jpg', 'Breakfast');
-  let bubblegum = new Product('assets/bubblegum.jpg', 'Bubblegum');
-  let chair = new Product('assets/chair.jpg', 'Chair');
-  let cthulu = new Product('assets/cthulhu.jpg', 'Cthulhu');
-  let dogDuck = new Product('assets/dog-duck.jpg', 'Dog-Duck');
-  let dragon = new Product('assets/dragon.jpg', 'Dragon');
-  let pen = new Product('assets/pen.jpg', 'Pen');
-  let petSweep = new Product('assets/pet-sweep.jpg', 'Pet Sweep');
-  let scissors = new Product('assets/scissors.jpg', 'Scissors');
-  let shark = new Product('assets/shark.jpg', 'Shark');
-  let sweep = new Product('assets/sweep.png', 'Sweep');
-  let tauntaun = new Product('assets/tauntaun.jpg', 'Taun-Taun');
-  let unicorn = new Product('assets/unicorn.jpg', 'Unicorn');
-  let waterCan = new Product('assets/water-can.jpg', 'Water Can');
-  let wineGlass = new Product('assets/wine-glass.jpg', 'Wine Glass');
-  state.allProducts.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, waterCan, wineGlass);
+  let bag = new Product("assets/bag.jpg", "Bag");
+  let banana = new Product("assets/banana.jpg", "Banana");
+  let bathroom = new Product("assets/bathroom.jpg", "Bathroom");
+  let boots = new Product("assets/boots.jpg", "Boots");
+  let breakfast = new Product("assets/breakfast.jpg", "Breakfast");
+  let bubblegum = new Product("assets/bubblegum.jpg", "Bubblegum");
+  let chair = new Product("assets/chair.jpg", "Chair");
+  let cthulu = new Product("assets/cthulhu.jpg", "Cthulhu");
+  let dogDuck = new Product("assets/dog-duck.jpg", "Dog-Duck");
+  let dragon = new Product("assets/dragon.jpg", "Dragon");
+  let pen = new Product("assets/pen.jpg", "Pen");
+  let petSweep = new Product("assets/pet-sweep.jpg", "Pet Sweep");
+  let scissors = new Product("assets/scissors.jpg", "Scissors");
+  let shark = new Product("assets/shark.jpg", "Shark");
+  let sweep = new Product("assets/sweep.png", "Sweep");
+  let tauntaun = new Product("assets/tauntaun.jpg", "Taun-Taun");
+  let unicorn = new Product("assets/unicorn.jpg", "Unicorn");
+  let waterCan = new Product("assets/water-can.jpg", "Water Can");
+  let wineGlass = new Product("assets/wine-glass.jpg", "Wine Glass");
+  state.allProducts.push(
+    bag,
+    banana,
+    bathroom,
+    boots,
+    breakfast,
+    bubblegum,
+    chair,
+    cthulu,
+    dogDuck,
+    dragon,
+    pen,
+    petSweep,
+    scissors,
+    shark,
+    sweep,
+    tauntaun,
+    unicorn,
+    waterCan,
+    wineGlass
+  );
 }
 
 // Initialize the app by creating the big list of products with images and names
